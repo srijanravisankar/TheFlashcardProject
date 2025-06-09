8cd8e5239016f472fd466cb4a10415f9d4bec8d2
Srijan Ravisankar <srijanvr@gmail.com>
2025-06-09 23:35:42 +0530

    added some docstring

--- flashcard.py
+++ flashcard.py
@@ -1,5 +1,11 @@
 
 class Flashcard():
+    """Represents a single flashcard with front and back."""
 
-    def __init__():
-        pass
+    def __init__(self, front, back):
+        """_summary_
+
+        Args:
+            front (str): The text displayed on the front of the flashcard.
+            back (str): The text displayed on the back of the flashcard.
+        """
