0cb52722dd2b1f481f20bc49d4ab1a5dbf7fb638
Srijan Ravisankar <srijanvr@gmail.com>
2025-07-07 18:16:44 +0530

    Add update functionality to flashcards

--- frontend/src/components/Deck.jsx
+++ frontend/src/components/Deck.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Box, Typography, Paper, CircularProgress, IconButton } from '@mui/material';
+import { Box, Typography, Paper, CircularProgress, IconButton, TextField } from '@mui/material';
 import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import SaveRoundedIcon from '@mui/icons-material/SaveRounded';
 
 import api from '../api';
 
 export default function Deck() {
   const { deckId } = useParams();
-  const [cards, setCards] = useState();
+
+  const [cards, setCards] = useState(null);
+  const [edit, setEdit] = useState(false);
+  const [frontText, setFrontText] = useState('');
+  const [backText, setBackText] = useState('');
+
 
   useEffect(() => getCards(), [deckId]);
 
@@ -27,6 +33,20 @@ export default function Deck() {
       .catch((err) => console.error('Failed to delete card:', err));
   }
 
+  const updateCard = (id, updatedCard) => {
+    api
+      .put(`/cards/${id}`, {
+          deck_id: updatedCard.deckId, 
+          front_text: updatedCard.frontText, 
+          back_text: updatedCard.backText
+        })
+      .then(() => {
+        getCards();
+        setEdit(false);
+      })
+      .catch((err) => console.error('Failed to update card:', err));
+  }
+
   return (
     <Box sx={{ padding: 2 }}>
       <Typography variant="h5" gutterBottom>
@@ -34,29 +54,52 @@ export default function Deck() {
       </Typography>
 
       {
-        cards ? (
+        cards === null ?  <CircularProgress /> : (
           cards.length === 0 ? (
             <Typography>No cards found.</Typography>
           ) : (
             cards.map((card) => (
-              <Paper
-                key={card.id}
-                onClick={() => console.log('card clicked', card.id)}
-                sx={{padding: 2, marginBottom: 2, backgroundColor: '#f9f9f9', boxShadow: 1, cursor: 'pointer','&:hover': { boxShadow: 3 },}}
-              >
-                <Typography><strong>Q:</strong> {card.front_text}</Typography>
-                <Typography><strong>A:</strong> {card.back_text}</Typography>
-                <IconButton onClick={() => deleteCard(card.id)}>
-                  <DeleteOutlineRoundedIcon />
-                </IconButton>
-                <IconButton onClick={() => console.log('update button clicked', card.id)}>
-                  <EditRoundedIcon />
-                </IconButton>
+              <Paper key={card.id} onClick={() => console.log('card clicked', card.id)} sx={paperStyle}>
+                {!edit ? <>
+                    <Box sx={editCardStyle}>
+                      <Typography><strong>Q:</strong> {card.front_text}</Typography>
+                      <Typography><strong>A:</strong> {card.back_text}</Typography>
+                      <Box>
+                        <IconButton onClick={() => setEdit(true)}><EditRoundedIcon /></IconButton>
+                        <IconButton onClick={() => deleteCard(card.id)}><DeleteOutlineRoundedIcon /></IconButton>
+                      </Box>
+                    </Box>
+                  </> : <>
+                    <Box sx={editCardStyle}>
+                      <TextField label="Front text" onChange={(e) => setFrontText(e.target.value)} />
+                      <TextField label="Back text" onChange={(e) => setBackText(e.target.value)} />
+                      <Box>
+                        <IconButton onClick={() => updateCard(card.id, {deckId, frontText, backText})}><SaveRoundedIcon /></IconButton>
+                        <IconButton onClick={() => deleteCard(card.id)}><DeleteOutlineRoundedIcon /></IconButton>
+                      </Box>
+                    </Box>
+                  </>
+                }
               </Paper>
             ))
           )
-        ) : <CircularProgress />
+        )
       }
     </Box>
   );
+}
+
+const paperStyle = {
+  padding: 2, 
+  marginBottom: 2, 
+  backgroundColor: '#f9f9f9', 
+  boxShadow: 8, 
+  cursor: 'pointer',
+  '&:hover': { boxShadow: 3 }
+}
+
+const editCardStyle = { 
+  display: 'flex', 
+  flexDirection: 'column', 
+  gap: 2 
 }
