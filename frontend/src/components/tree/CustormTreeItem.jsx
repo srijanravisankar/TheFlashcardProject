94fce4917960c13036dbcbe3006db1ca52d212bd
Srijan Ravisankar <srijanvr@gmail.com>
2025-07-09 17:48:39 +0530

    Add crud buttons in tree structure without implementation

--- frontend/src/components/tree/CustormTreeItem.jsx
+++ frontend/src/components/tree/CustormTreeItem.jsx
@@ -3,6 +3,9 @@ import { forwardRef } from 'react';
 import { Box, IconButton } from '@mui/material';
 import FolderRoundedIcon from '@mui/icons-material/FolderRounded';
 import LayersRoundedIcon from '@mui/icons-material/LayersRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 
 import { useTreeItem } from '@mui/x-tree-view/useTreeItem';
 import {
@@ -38,26 +41,47 @@ export const CustomTreeItem = forwardRef(function CustomTreeItem(props, ref) {
   return (
     <TreeItemProvider {...getContextProviderProps()}>
       <TreeItemRoot {...getRootProps(other)}>
-        <TreeItemContent {...getContentProps()}>
+        <TreeItemContent {...getContentProps()} sx={{
+            '&:hover .tree-actions': { display: 'flex' },
+            display: 'flex',
+            alignItems: 'center',
+            height: 30
+          }}>
+
           <TreeItemIconContainer {...getIconContainerProps()}>
             <TreeItemIcon status={status} />
           </TreeItemIconContainer>
-          <Box sx={{ flexGrow: 1, display: 'flex', gap: 1 }}>
+
+          <Box sx={{ flexGrow: 1, display: 'flex', gap: 1, alignItems: 'center' }}>
             {item.type === 'folder' ? <FolderRoundedIcon /> : <LayersRoundedIcon />}
             <TreeItemCheckbox {...getCheckboxProps()} />
             <TreeItemLabel {...getLabelProps()} />
-            <IconButton
-              edge="end"
-              size="small"
-              sx={{ marginLeft: 'auto', color: 'gray' }}
-              onClick={(e) => {
-                e.stopPropagation();
-                console.log(`Delete ${item.id}`);
-              }}
-            ></IconButton>
+
+            <Box className="tree-actions" sx={{ display: 'none', gap: 0.5 }}>
+              <IconButton edge="end" size="small" sx={{ marginLeft: 'auto', color: 'gray' }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  console.log(`Delete ${item.id}`);
+                }}
+              ><AddCircleRoundedIcon /></IconButton>
+              <IconButton edge="end" size="small" sx={{ marginLeft: 'auto', color: 'gray' }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  console.log(`Delete ${item.id}`);
+                }}
+              ><EditRoundedIcon /></IconButton>
+              <IconButton edge="end" size="small" sx={{ marginLeft: 'auto', color: 'gray' }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  console.log(`Delete ${item.id}`);
+                }}
+              ><DeleteRoundedIcon /></IconButton>
+            </Box>
+
           </Box>
           <TreeItemDragAndDropOverlay {...getDragAndDropOverlayProps()} />
         </TreeItemContent>
+        
         {children && <TreeItemGroupTransition {...getGroupTransitionProps()} />}
       </TreeItemRoot>
     </TreeItemProvider>
