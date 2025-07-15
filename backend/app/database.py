95d689881fe7bf582f1a214f602abbd3cd11f7f0
Srijan Ravisankar <srijanvr@gmail.com>
2025-07-15 16:18:48 +0530

    Change Postgres to SQLite

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,31 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:Srijan22$$@localhost:5433/flashcard_db"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# def get_db():
+#     db = Session()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:Srijan22$$@localhost:5433/flashcard_db"
+# Path to SQLite DB file in current directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "../flashcard_db.sqlite3")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
